91a1c51604d30bd9b7fe10e8a12e3cfba0e2d43d
Fukushima <toshiki.reb@gmail.com>
2022-04-25 11:03:52 +0900

    MVTモデルにコード分割

---
+++ my_app/models.py
@@ -0,0 +1,55 @@
+import mysql.connector
+
+def connect_db():
+    conn = mysql.connector.connect(
+        host = 'localhost',
+        user = 'user21',
+        passwd = 'user21(PASS)',
+        db = 'employee',
+        auth_plugin="mysql_native_password"
+    )
+    return conn
+
+# select文を一行返すメソッド
+def select_one(sql, *args):
+    row = None
+    conn = connect_db()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(sql, [arg for arg in args]) # なぜかlist(args)がエラーになる
+        row = cursor.fetchone()
+    except(mysql.connector.errors.ProgrammingError) as e:
+            print(e)
+    finally:
+        cursor.close()
+        conn.close()
+    return row
+
+# select文を複数行返すメソッド
+def select_all(sql, *args):
+    rows = None
+    conn = connect_db()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(sql, [arg for arg in args]) # なぜかlist(args)がエラーになる
+        rows = cursor.fetchall()
+    except(mysql.connector.errors.ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+    return rows
+
+# insert文, delete文を実行するメソッド
+# update文はまだ試していない
+def change_tbl(sql, *args):
+    conn = connect_db()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(sql, [arg for arg in args]) # なぜかlist(args)がエラーになる
+        conn.commit()
+    except(mysql.connector.errors.ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
