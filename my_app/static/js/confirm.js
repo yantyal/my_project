063700984d398aa9ce6fc408c631e8b05d45c5f7
Fukushima <toshiki.reb@gmail.com>
2022-04-25 16:07:06 +0900

    確認ダイアログ追加

--- my_app/static/js/confirm.js
+++ my_app/static/js/confirm.js
@@ -8,3 +8,25 @@ function delete_check(){
         return false;
     }
 }
+
+function add_check(){
+    if(window.confirm('ユーザー登録しますか？')){
+        window.alert('登録されました');
+        return true;
+    }
+    else{
+        window.alert('キャンセルされました');
+        return false;
+    }
+}
+
+function edit_check(){
+    if(window.confirm('登録情報を変更しますか？')){
+        window.alert('変更されました');
+        return true;
+    }
+    else{
+        window.alert('キャンセルされました');
+        return false;
+    }
+}
