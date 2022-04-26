b4110f886f9bea20ad0fa74845c068cf33dc4f33
Fukushima <toshiki.reb@gmail.com>
2022-04-26 13:41:43 +0900

    addコード分割

--- my_app/views.py
+++ my_app/views.py
@@ -8,22 +8,24 @@ app = Flask(__name__)
 app.secret_key = 'abcdefghijklmn'
 app.permanent_session_lifetime = timedelta(minutes=3) # セッションの生存時間は3分
 
+
 @app.route('/')
 def index():
     return redirect(url_for('login'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         if 'name' in session:
             return redirect(url_for('list'))
-        else:
-            end = time.time()
-            if 'start' in session:
-                # セッションに残すエラー文の生存時間は1秒
-                if end - session['start'] >= 1:
-                    session.clear()
-            return render_template('login.html')
+
+        end = time.time()
+        if 'start' in session:
+            # セッションに残すエラー文の生存時間は1秒
+            if end - session['start'] >= 1:
+                session.clear()
+        return render_template('login.html')
 
     if request.method == 'POST':
         mail_address = request.form['mail_address']
@@ -50,16 +52,18 @@ def login():
         session['start'] = time.time()
         return redirect(url_for('login'))
 
+
 @app.route('/user/list', methods=['GET','POST'])
 def list():
     session.pop('errors', None)
+
     if 'name' not in session:
         return redirect(url_for('login'))
 
     if request.method == 'GET':
         sql = issue_sql('list')
         rows = select_all(sql)
-    # POSTで受け取るのは社員の検索時
+    # 社員の検索時にPOSTで受け取る
     if request.method == 'POST':
         employee_id = request.form['employee_id']
         name = request.form['name']
@@ -84,35 +88,46 @@ def list():
 
     return render_template('list.html')
 
+
 @app.route('/user/add', methods=['GET', 'POST'])
 def add():
-    if 'name' in session:
-        if request.method == 'POST':
-            name = request.form['name']
-            belong_id = request.form['belong_id']
-            mail_address = request.form['mail_address']
-            password = request.form['password']
-            management = request.form.getlist('management')
-            if len(management) != 0:
-                management = management[0]
-            sql = issue_sql('login')
-            row = select_one(sql, mail_address, password)
-            if row is None:
-                if management == 'Y':
-                    sql = issue_sql('add', ["0"])
-                    change_tbl(sql, name, belong_id, mail_address, password, management)
-                else:
-                    sql = issue_sql('add', ["1"])
-                    change_tbl(sql, name, belong_id, mail_address, password)
-            else:
-                error_messages = create_error_messages('add')
-                return render_template('add.html', error_messages=error_messages)
-            return redirect(url_for('list'))
-        else:
-            error_messages = []
-            return render_template('add.html', error_messages=error_messages)
-    else:
+    if 'name' not in session:
         return redirect(url_for('login'))
+
+    if request.method == 'GET':
+        end = time.time()
+        if 'start' in session:
+            # セッションに残すエラー文の生存時間は1秒
+            if end - session['start'] >= 1:
+                session.pop('errors', None)
+                session.pop('start', None)
+        return render_template('add.html')
+    # 新規登録時にPOSTで受け取る
+    if request.method == 'POST':
+        name = request.form['name']
+        belong_id = request.form['belong_id']
+        mail_address = request.form['mail_address']
+        password = request.form['password']
+        management = request.form.getlist('management')
+        if len(management) != 0:
+            management = management[0]
+        sql = issue_sql('login')
+        row = select_one(sql, mail_address, password)
+
+    if row is not None:
+        session['errors'] = create_error_messages('add')
+        session['start'] = time.time()
+        return redirect(url_for('add'))
+
+    if management == 'Y':
+        sql = issue_sql('add', ["0"])
+        change_tbl(sql, name, belong_id, mail_address, password, management)
+    else:
+        sql = issue_sql('add', ["1"])
+        change_tbl(sql, name, belong_id, mail_address, password)
+
+    return redirect(url_for('list'))
+
 
 @app.route('/user/edit/<employee_id>', methods=['GET', 'POST'])
 def edit(employee_id):
@@ -128,6 +143,7 @@ def edit(employee_id):
         return render_template('edit.html', error_messages=error_messages, user=user)
     else:
         return redirect(url_for('login'))
+
 
 @app.route('/user/result', methods=['POST'])
 def edit_result():
@@ -154,12 +170,14 @@ def edit_result():
     else:
         return redirect(url_for('login'))
 
+
 @app.route('/user/delete/<employee_id>', methods=['POST'])
 def delete(employee_id):
     deleted_datetime = datetime.now().strftime('%Y-%m-%d')
     sql = issue_sql('delete')
     change_tbl(sql, deleted_datetime, employee_id)
     return redirect(url_for('list'))
+
 
 @app.route('/logout')
 def logout():
