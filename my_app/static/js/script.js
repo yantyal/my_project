9db32a8633e60191b6f379bb76768755d147d54b
Fukushima <toshiki.reb@gmail.com>
2022-04-28 17:54:09 +0900

    パスワードのマスク表示/非表示

--- my_app/static/js/script.js
+++ my_app/static/js/script.js
@@ -1,3 +1,29 @@
 var select = document.getElementById("belong_id");
 var number = document.getElementById("user_belong_id").value
 select.options[number].selected = true
+
+function password_mask(obj) {
+    console.log(obj)
+    var txtPass = document.getElementById(('emp_id' + obj));
+    var btnEye = document.getElementById(('btn_emp_id' + obj));
+    if (txtPass.type === "text") {
+        txtPass.type = "password";
+        console.log(btnEye.className)
+        btnEye.className = "fa fa-eye";
+    } else {
+        txtPass.type = "text";
+        console.log(btnEye.className)
+        btnEye.className = "fa fa-eye-slash";
+    }
+}
+function password_mask(){
+    var txtPass = document.getElementById('login');
+    var btnEye = document.getElementById('btn_login');
+    if (txtPass.type === "text") {
+        txtPass.type = "password";
+        btnEye.className = "fa fa-eye";
+    } else {
+        txtPass.type = "text";
+        btnEye.className = "fa fa-eye-slash";
+    }
+}
