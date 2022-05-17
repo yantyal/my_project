70c13015d065c6fe98520fac78078f1ba6f23a41
Fukushima <toshiki.reb@gmail.com>
2022-05-17 09:39:23 +0900

    削除時の確認ダイアログをクラス名から行う

--- my_app/static/js/confirm.js
+++ my_app/static/js/confirm.js
@@ -1,36 +1,8 @@
-function delete_check(){
-    if(window.confirm('削除しますか？')){
-        window.alert('削除を受けつけました');
-        return true;
-    }
-    else{
-        window.alert('キャンセルされました');
-        return false;
-    }
-}
-
-function add_check(){
-    if(window.confirm('ユーザー登録しますか？')){
-        window.alert('登録を受けつけました');
-        return true;
-    }
-    else{
-        window.alert('キャンセルされました');
-        return false;
-    }
-}
-
-function edit_check(){
-    if(window.confirm('登録情報を変更しますか？')){
-        window.alert('変更を受けつけました');
-        return true;
-    }
-    else{
-        window.alert('キャンセルされました');
-        return false;
-    }
-}
-
-function sort_clear(){
-    location.href = "./list";
+var deletes = document.getElementsByClassName('delete');
+for(let i = 0; i < deletes.length; i++){
+    deletes[i].addEventListener('click', function(e){
+        if(!confirm('削除しますか?')){
+            e.preventDefault();
+        }
+    });
 }
