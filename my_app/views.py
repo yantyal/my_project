8af581dfe43270a0e2fc7e358c1dbb7c12b0bd73
Fukushima <toshiki.reb@gmail.com>
2022-05-17 09:47:27 +0900

    デザイン変更に伴う変数名の変更

--- my_app/views.py
+++ my_app/views.py
@@ -66,13 +66,18 @@ def list():
         return render_template('list.html')
     # 社員の検索時にPOSTで受け取る
     if request.method == 'POST':
-        employee_id = request.form['employee_id']
-        name = request.form['name']
-        name = '%' + name + '%'
-        belong_id = request.form['belong_id']
-        sql_condition = create_sql_condition(employee_id, name, belong_id)
+        sort_employee_id = None
+        sort_name = None
+        belong_id = '0'
+        if 'sort' in request.form:
+            sort = request.form['sort']
+            sort_employee_id = sort
+            sort_name = '%' + sort + '%'
+        if 'belong_id' in request.form:
+            belong_id = request.form['belong_id']
+        sql_condition = create_sql_condition(sort_employee_id, sort_name, belong_id)
         sql = issue_sql('sort', sql_condition)
-        rows = select_all(sql, employee_id, name, belong_id)
+        rows = select_all(sql, sort, sort_name, belong_id)
 
     if rows is None:
         session['errors'] = create_error_messages('sort')
