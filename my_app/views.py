782b5243125a624b8a28884021afdea47c7b1224
Fukushima <toshiki.reb@gmail.com>
2022-05-18 13:08:51 +0900

    ブラウザにログイン情報を保存するためクッキーを実装

--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, make_response
 from flask import request, session
 from datetime import datetime
-import time
+import time, json
 from my_app.models import (check_error_in_session, create_app, create_error_messages, create_sql_condition, create_users,
 issue_table, save_file, select_one, select_all, change_tbl, issue_sql, create_hash)
 
@@ -19,12 +19,39 @@ def login():
         if 'name' in session:
             return redirect(url_for('list'))
 
-        check_error_in_session(session, 1)
-        return render_template('login.html')
+        user_info = request.cookies.get('user_info')
+        if user_info is None:
+            check_error_in_session(session, 1)
+            return render_template('login.html')
+
+        user_info = json.loads(user_info)
+        mail_address = user_info['mail_address']
+        password = user_info['password']
+        sql = issue_sql('login')
+        row = select_one(sql, mail_address, password)
+        table = issue_table('login')
+
+        # エラー発生時は1で、リダイレクト先を変える
+        redirect_number = 0
+        if row is not None:
+            for t, r in zip(table, row):
+                session[t] = r
+                if t == 'deleted_datetime' and r is not None:
+                    redirect_number = 1
+        else:
+            redirect_number = 1
+
+        if redirect_number == 0:
+            return redirect(url_for('list'))
+        else:
+            return render_template('login.html')
 
     if request.method == 'POST':
         mail_address = request.form['mail_address']
         password = create_hash(request.form['password'])
+        check_cookie = request.form.getlist('check_cookie')
+        if len(check_cookie) != 0:
+            check_cookie = check_cookie[0]
         sql = issue_sql('login')
         row = select_one(sql, mail_address, password)
         table = issue_table('login')
@@ -39,14 +66,21 @@ def login():
     else:
         redirect_number = 1
 
-    if redirect_number == 0:
-        return redirect(url_for('list'))
-    else:
+    if redirect_number == 1:
         session.clear()
         session['errors'] = create_error_messages('login')
         session['start'] = time.time()
         return redirect(url_for('login'))
 
+    if check_cookie == 'ok':
+        max_age = 30 # クッキーの生存時間は30秒
+        expires = int(datetime.now().timestamp()) + max_age
+        response = make_response(redirect(url_for('list')))
+        user_info = {'mail_address': mail_address, 'password': password}
+        response.set_cookie("user_info", value=json.dumps(user_info), expires=expires)
+        return response
+    else:
+        return redirect(url_for('list'))
 
 @app.route('/user/list', methods=['GET','POST'])
 def list():
