70a3759bde0d3f50cae8ea7abdd365171bfb6928
Fukushima <toshiki.reb@gmail.com>
2022-05-20 16:09:17 +0900

    SQLとエラーが発生する箇所でログを残せるようにする（社員一覧リスト）

--- my_app/views/list.py
+++ my_app/views/list.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for
 from flask import request, session, current_app
 import time
 from my_app.models import (check_error_in_session, check_success_in_session, create_error_messages,
-create_sql_condition, create_users, issue_table, select_all, issue_sql)
+create_sql_condition, create_users, issue_table, select_all, issue_sql, formatter)
 
 list_bp = Blueprint('list', __name__, url_prefix='/user', template_folder='my_app.templates')
 
@@ -25,6 +25,8 @@ def list():
         rows = select_all(DB_INFO, sql)
         table = issue_table('list')
         session["users"] = create_users(table, rows)
+        formatter.set_employee_id(session)
+        current_app.logger.info(sql)
         return render_template('list.html')
     # 社員の検索時にPOSTで受け取る
     if request.method == 'POST':
@@ -42,13 +44,21 @@ def list():
         sql_condition = create_sql_condition(sort_employee_id, sort_name, belong_id)
         sql = issue_sql('sort', sql_condition)
         rows = select_all(DB_INFO, sql, sort_employee_id, sort_name, belong_id)
+        formatter.set_employee_id(session)
+        current_app.logger.info(sql)
 
     if rows is None:
         session['errors'] = create_error_messages('sort')
         session['start'] = time.time()
+        formatter.set_employee_id(session)
+        current_app.logger.info('Logout.')
+        return redirect(url_for('list.list'))
     if len(rows) == 0:
         session['errors'] = create_error_messages('list')
         session['start'] = time.time()
+        formatter.set_employee_id(session)
+        current_app.logger.info('Logout.')
+        return redirect(url_for('list.list'))
 
     table = issue_table('list')
     session["users"] = create_users(table, rows)
