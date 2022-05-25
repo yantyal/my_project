4b77340b1cef6c31a824accf050f849a219bdfc6
Fukushima <toshiki.reb@gmail.com>
2022-05-25 09:58:52 +0900

    登録できないファイルが選択された場合、エラーメッセージを出す

--- my_app/static/js/preview.js
+++ my_app/static/js/preview.js
@@ -3,7 +3,16 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById('file').addEventListener('change', function (e) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            document.getElementById("preview").setAttribute('src', e.target.result);
+            function is_check_file_data(){
+                return  e.target.result.indexOf('image/') === 5;
+            }
+            if(is_check_file_data()){
+                document.getElementById("preview").setAttribute('src', e.target.result);
+                document.getElementById("error-message").innerText = ''
+            }else{
+                document.getElementById("preview").setAttribute('src', '/static/img/noimage.png');
+                document.getElementById("error-message").innerText = '登録できないファイルが選択されました。'
+            }
         }
         reader.readAsDataURL(e.target.files[0]);
         });
