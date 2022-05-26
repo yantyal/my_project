5ffcedf10d122d79812780685ee1ab728bfe6daf
Fukushima <toshiki.reb@gmail.com>
2022-05-26 14:24:57 +0900

    SQLの名前とテーブルの名前を定義

--- my_app/enum.py
+++ my_app/enum.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from my_app.views.edit import change_password
+
 # render_templateにより画面遷移先HTML
 class transition_render_template_target(Enum):
     LOGIN = 'login.html'
@@ -27,3 +29,22 @@ class Login_user_info(Enum):
     NAME = 'name'
     DELETED_DATETIME = 'deleted_datetime'
     MANAGEMENT = 'management'
+
+# 呼び出されるSQLの名前
+class sql_name(Enum):
+    LOGIN = 'login'
+    LIST = 'list'
+    SORT = 'sort'
+    ADD_CHECK = 'add_check'
+    ADD = 'add'
+    EDIT_USER_INFO = 'edit_user_info'
+    EDIT_CHECK = 'edit_check'
+    EDIT = 'edit'
+    CHANGE_PASSWORD = 'change_password'
+    DELETE = 'delete'
+
+# 呼び出されるテーブルの名前
+class table_name(Enum):
+    LOGIN = 'login'
+    LIST = 'list'
+    EDIT = 'edit'
